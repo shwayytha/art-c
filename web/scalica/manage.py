fa58f23387c3cf146624b17c2b16b6de1c910d1e
shwayytha <sg3688@nyu.edu>
2016-12-19 16:51:32 -0500

    Update manage.py

--- web/scalica/manage.py
+++ web/scalica/manage.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+    
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
